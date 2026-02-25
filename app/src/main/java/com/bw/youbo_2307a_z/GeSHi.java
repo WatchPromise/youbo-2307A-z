57cc23f26e0f59e8fb6176921ca5e5e0e274d889
WatchPromise <2813832364@qq.com>
2026-02-25 15:53:44 +0800

    绍刚

--- app/src/main/java/com/bw/youbo_2307a_z/GeSHi.java
+++ app/src/main/java/com/bw/youbo_2307a_z/GeSHi.java
@@ -2,6 +2,6 @@ package com.bw.youbo_2307a_z;
 
 public class GeSHi {
     public static void main(String[] args) {
-        System.out.println("我嘞个雷");
+        System.out.println("我嘞个绍刚");
     }
 }
